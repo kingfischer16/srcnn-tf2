4edbd489ddc6e631378a0543023e40dfc8798f49
Lee <lee.m.fischer@gmail.com>
2020-11-18 20:24:27 +0100

    upscaling function successfully transferred to AWS Lambda

--- srcnn-api/handler.py
+++ srcnn-api/handler.py
@@ -44,6 +44,8 @@ class SRCNNDeployLite:
         self.input_details = self.interpreter.get_input_details()
         self.scaling_factor = scaling_factor
         self.edge_pad = edge_pad
+        print("SRCNNDeployLite object created.")
+        print(f"Input details: {self.input_details}")
     
     def _bicubic_upscale(self):
         """
@@ -52,12 +54,14 @@ class SRCNNDeployLite:
         output_image_size = (int(self.image.size[0]*self.scaling_factor), int(self.image.size[1]*self.scaling_factor))
         self.upscaled_image = self.image.resize(size=output_image_size, resample=Image.BICUBIC)
         self.upscaled_image = np.array(self.upscaled_image) / 255.0
+        print("Pre-upscaling complete.")
     
     def _pad_image(self):
         """
         Applied zero padding to the edge of the image.
         """
         self.padded_image = np.pad(self.upscaled_image, ((self.edge_pad, self.edge_pad), (self.edge_pad, self.edge_pad), (0, 0)))
+        print("Image padded.")
     
     def enhance(self, image):
         """
@@ -69,7 +73,7 @@ class SRCNNDeployLite:
         self._pad_image()
         x_image = np.array([self.padded_image], dtype=np.float32)
         im_size = x_image.shape
-        
+        print(f"Sending image to predictor. Input data size: {im_size}")
         # Resize input tensor and allocate.
         self.interpreter.resize_tensor_input(
             self.input_details[0]['index'],
@@ -106,7 +110,7 @@ def upload_to_s3(bucket, key, image):
     out_image.seek(0)
 
     response =s3.put_object(
-        ACL='private',
+        ACL='public-read',
         Body=out_image,
         Bucket=bucket,
         ContentType='image/png',
@@ -117,14 +121,16 @@ def upload_to_s3(bucket, key, image):
     url = f'{s3.meta.endpoint_url}/{bucket}/{key}'
     return url
 
-def image_upscale(image):
+def apply_upscaling(image):
     """
     Upscale the image given the input image and scaling factor.
+    Applies 2x scaling by default in the S3 example.
     """
     model_path = 'lite_models/20201107_basic_2x_srcnn.tflite'
     srcnn_model = SRCNNDeployLite(model_location=model_path, scaling_factor=2, edge_pad=6)
     output_arr = srcnn_model.enhance(image)
-    output_image = Image.fromarray(output_arr)
+    print(f"Image upscaled successfully. New image size: {output_arr.shape}, {type(output_arr)}")
+    output_image = Image.fromarray((output_arr*255).astype(np.uint8))
     return output_image
 
 def image_upscaler(event, context):
@@ -132,16 +138,22 @@ def image_upscaler(event, context):
     Handler method.
     """
     # parse event, just for understanding what is in the event
+    print("Function invoked.")
     print(event)
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     # only create a thumbnail on non-thumbnail images
     if not key.endswith("_upscaled.png"):
+        print(f"Upscaling started for image: {key}")
         # get the image
         image = get_s3_image(bucket, key)
+        print("Image retrieved from S3.")
         # upscale image
-        upscaled_image = image_upscale(image)
+        upscaled_image = apply_upscaling(image)
+        print("Image upscaled.")
         # upload the file
         upscaled_key = key.rsplit('.', 1)[0] + '_upscaled.png'
+        print(f"Returned image labelled: {upscaled_key}")
         url = upload_to_s3(bucket, upscaled_key, upscaled_image)
+        print("Image written to S3.")
         return url
